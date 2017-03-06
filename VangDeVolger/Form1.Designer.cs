85db611bc76f7624cd69bc04d01465c4058fcce0
Leon Hubert <leon-10@live.nl>
2017-03-06 12:43:25 +0100

    Resized added label and no icon

--- VangDeVolger/Form1.Designer.cs
+++ VangDeVolger/Form1.Designer.cs
@@ -28,9 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 243);
+            this.Name = "Form1";
+            this.ShowIcon = false;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- VangDeVolger/Program.cs
+++ VangDeVolger/Program.cs
@@ -17,6 +17,8 @@ namespace VangDeVolger
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Label label = new Label();
+            label.Text = "Vang de volger!";
         }
     }
 }
