50ca9291f3a32bb28e2c36c821e83ef284b88752
ChristianRuigrok <christian@ruigrok.info>
2017-03-08 14:31:19 +0100

    Added outdated comments

--- VangDeVolger/Game.cs
+++ VangDeVolger/Game.cs
@@ -180,7 +180,7 @@ namespace VangDeVolger
         /// <param name="e"></param>
         private void Timer1_Tick(object sender, EventArgs e)
         {
-
+            
         }
 
         /// <summary>
@@ -193,70 +193,5 @@ namespace VangDeVolger
             _player.Move(ref Blocks, e);
             label2.Text = "Player: X: " + _player.Pb.Location.X + " Y: " + _player.Pb.Location.Y;
         }
-
-        /*
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            const int speed = 2;
-            _playerX = pictureBox1.Location.X;
-            _playerY = pictureBox1.Location.Y;
-
-            _enemyX = pictureBox2.Location.X;
-            _enemyY = pictureBox2.Location.Y;
-
-            // Check of de player en enemy elkaar raken
-            if (!pictureBox1.Bounds.IntersectsWith(pictureBox2.Bounds))
-            {
-                // Check of de enemy ee blok raakt
-                if (!pictureBox2.Bounds.IntersectsWith(pictureBox3.Bounds))
-                {
-                    if (_enemyX < _playerX)
-                    {
-                        _prevEnemyX = _enemyX;
-                        _enemyX += speed;
-                    }
-                    if (_enemyX > _playerX)
-                    {
-                        _prevEnemyX = _enemyX;
-                        _enemyX -= speed;
-                    }
-                    if (_enemyY < _playerY)
-                    {
-                        _prevEnemyY = _enemyY;
-                        _enemyY += speed;
-                    }
-                    if (_enemyY > _playerY)
-                    {
-                        _prevEnemyY = _enemyY;
-                        _enemyY -= speed;
-                    }
-                }
-                // Vind de korste weg langs het blok naar de speler
-                else
-                {
-                    // Omhoog erlangs
-                    if (_playerY < _enemyY)
-                    {
-                        _enemyY -= speed;
-                    }
-                    // Omlaag erlangs
-                    if (_playerY > _enemyY)
-                    {
-                        _enemyY += speed;
-                    }
-                    // dit is kutter dan kut.
-                }
-            }
-            else
-            {
-                timer1.Stop();
-                MessageBox.Show("Game over!");
-            }
-            label1.Text = "Enemy: X: " + _enemyX + " Y: " + _enemyY;
-            var p = new Point(_enemyX, _enemyY);
-            pictureBox2.Location = p;
-        }
-        */
     }
 }
