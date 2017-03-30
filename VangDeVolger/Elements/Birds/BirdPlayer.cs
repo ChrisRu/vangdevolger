b350a05cf0b8bcba62b4e9283e4a6b25c7ba0b71
Christian Ruigrok <christian@ruigrok.info>
2017-03-30 18:05:34 +0200

    Removed unecessary check in player

--- VangDeVolger/Elements/Birds/BirdPlayer.cs
+++ VangDeVolger/Elements/Birds/BirdPlayer.cs
@@ -28,10 +28,6 @@ namespace VangDeVolger.Elements.Birds
             {
                 if (nextSpot.Element == null || nextSpot.Element.CanMove(direction))
                 {
-                    if (Parent.Neighbors[direction].Element is Enemy)
-                    {
-                        MessageBox.Show("GameOver");
-                    }
                     Move(direction);
                     return true;
                 }
